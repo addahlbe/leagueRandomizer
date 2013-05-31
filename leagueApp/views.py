d3dd311351847ce4f091b9a6d583ca8b0b610742
Anthony Dahlberg <anthony-dahlberg@uiowa.edu>
2013-05-31 12:29:20 -0500

    Header

--- leagueApp/views.py
+++ leagueApp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from models import Hero
 
 
 def home(request):
-    return render(request, 'home.html',)
+    all_heros = Hero.objects.all()
+    mid = Hero.objects.filter(primary_role="Mid")
+    top = Hero.objects.filter(primary_role="Top")
+    adc = Hero.objects.filter(primary_role="ADC")
+    support = Hero.objects.filter(primary_role="Support")
+    jungle = Hero.objects.filter(primary_role="Jungle")
+    return render(request, 'home.html', {
+        'all_heros': all_heros,
+        'mid': mid,
+        'top': top,
+        'adc': adc,
+        'support': support,
+        'jungle': jungle})
 
 
 def signup(request):
